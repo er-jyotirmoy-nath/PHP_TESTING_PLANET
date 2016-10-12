747873e9f0deedc586ecb31271959883752fa0ea
jyotirmoy-err-2016 <er.jyotirmoy.nath@gmail.com>
2016-10-12 14:05:55 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -7,7 +7,13 @@ try
 {
   $con = new PDO("mysql:host=$host;dbname=$db_name",$username,$password);
   $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRORMODE_EXCEPTION);
-  $sql = "":
+  $sql = "create table new_table(
+  ID INT(16) ATU_INCREMENT PRIMARY_KEY,
+  username VARCHAR(255) NOT NULL,
+  firstname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) ,
+  reg_date TIMESTAMP
+  );":
   $con->exec($sql);
   echo "Donesuccessfully";
   
