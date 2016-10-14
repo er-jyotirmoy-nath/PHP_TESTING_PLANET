42b00d504105bbb320ba8abefb33259d1b5c75f7
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-14 17:32:53 +0530

    Update Jquery_to_update_div.php

--- Jquery_to_update_div.php
+++ Jquery_to_update_div.php
@@ -1,1 +1,23 @@
 
+<!DOCTYPE html>
+<head>
+<title>Untitled Document</title>
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+
+<script>
+    $(document).ready(function(){
+        setInterval(function() {
+            $("#latestData").load("getLatestData.php");
+        }, 10000);
+    });
+
+</script>
+</head>
+
+<body>
+    <div id = "latestData">
+
+    </div>
+</body>
+</html>
