493b65751b218996a54d31106fe3567fbda44ff4
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-14 15:28:42 +0530

    Update File_handling.php

--- File_handling.php
+++ File_handling.php
@@ -1,1 +1,36 @@
 
+<?php
+$directory = $_POST['Directory'];
+
+if($handle = opendir($directory)){
+  echo "Looking into <b>".$directory."</b>";
+  
+  while($file = readdir($handle))
+  {
+    echo "<b><a href =?id=".base64_encode($file).">".$file."</a></b></br>";
+    
+  }
+}
+else
+{
+  echo 'No such directory!!';
+}
+if(isset($_GET['id']) && !empty($_GET['id']))
+{
+  $file_name = $_GET["id"];
+  if(file_exists($filename)){
+  $handle_file = fopen($file_name,"r");
+  $file_string = fread($handle_file,filesize($file_name));
+  echo '<form name="fileediter" method="POST" action = "'.$_SERVER['PHP_SELF'].'"><textarea cols=50 rows = 50>'.$file_string.'</textarea>';
+  echo '<input type="submit" name="edit" value="Edit" /><input type="submit" name="Delete" value="Delete" /></form>';
+  }
+  else{
+    echo 'File doesnt exist';
+  }
+  }
+  else
+  {
+    echo "No such file exists!!";
+  }
+}
+?>
