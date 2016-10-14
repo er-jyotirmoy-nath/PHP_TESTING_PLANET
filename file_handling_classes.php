4bfb2461594236eed36ed10f019a1be3c8e7b118
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-14 16:34:07 +0530

    Update file_handling_classes.php

--- file_handling_classes.php
+++ file_handling_classes.php
@@ -1,1 +1,31 @@
 
+<?php
+
+class file_handling{
+
+function public __construct($dirname){
+  if($handledir = opendir($dirname))
+  {
+    echo 'Reading the Directory \ '.$dirname;
+    while ($file = readdir($handledir))
+    {
+     echo "Files here are".$file."<br>"; 
+    }
+  }
+}
+  function public writetofile($filename)
+  {
+    
+  }
+  function public deletefile($filename)
+  {
+    
+  }
+  function public readafile($filename)
+  {
+    
+  }
+}
+
+
+?>
