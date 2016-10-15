a0587a3fe529eb31ad8a3ad4304383c0a7daa011
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-15 18:15:06 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,10 +1,7 @@
 <?php
-<<<<<<< HEAD
-if(isset($_POST["user_id"]) && !empty($_POST["user_id"]) && isset($_POST["user_pass"]) && !empty($_POST["user_pass"]) && isset($_POST["user_fname"]) && !empty($_POST["user_fname"]) && isset($_POST["user_lname"]) && !empty($_POST["user_lname"]))
-{
-	
-		
-=======
+
+
+
 class registeruser
 {
 	public $user_id;
@@ -43,7 +40,7 @@ class registeruser
 	}
 	public function insert()
 	{
-		if($this->checkuser($user_id)
+		if($this->checkuser($user_id))
 		{
 		try
 		{
@@ -72,39 +69,12 @@ class registeruser
 if(isset($_POST["user_id"]) && !empty($_POST["user_id"]) && isset($_POST["user_pass"]) && !empty($_POST["user_pass"]) && isset($_POST["user_fname"]) && !empty($_POST["user_fname"]) && isset($_POST["user_lname"]) && !empty($_POST["user_lname"]))
 {
 	
->>>>>>> origin/master
+
 		$user_id = mysql_real_escape_string($_POST["user_id"]);
 		$password = md5(mysql_real_escape_string($_POST["user_pass"]));
 		$fname = mysql_real_escape_string($_POST["user_fname"]);
 		$lname = mysql_real_escape_string($_POST["user_lname"]);
 		$usert = date("d M Y H:m:s",time());
-<<<<<<< HEAD
-		//if(!check_user_reg())
-		//{
-			try{
-			echo $password;
-		//require_once("connect.php");
-		$con = new PDO("mysql:host=localhost;dbname=a_database","root","");
-			$stmt = $con->prepare("insert into register_user(`user_id`, `password`, `first_name`, `last_name`, `reg_time`)  values(:user_id,:password,:fname,:lname,:utime)");
-			
-			$stmt->bindParam(":user_id",$user_id);
-			$stmt->bindParam(":password",$password,PDO::PARAM_STR,24);
-			$stmt->bindParam(":fname",$fname);
-			$stmt->bindParam(":lname",$lname);
-			$stmt->bindParam(":utime",$usert);
-			$stmt->execute();
-			//$con=null;
-			echo 'Registeration Successfull..<a href="index.php">Login</a>';
-			}
-			catch (Exception $ex){
-				echo $ex->getMessage();
-			}
-	//	}
-	//	else
-		//{
-	//		echo 'User exists'.'<a href="index.php">Login</a>';
-	//	}
-=======
 		$reg1 = new registeruser($user_id,$password,$fname,$lname,$usert);
 		if($reg1->insert())
 		{
@@ -115,11 +85,4 @@ if(isset($_POST["user_id"]) && !empty($_POST["user_id"]) && isset($_POST["user_p
 		{
 			echo 'User exists'.'<a href="index.php">Login</a>';
 		}
->>>>>>> origin/master
-	
-}
-else
-{
-	echo "You did not enter anything try again";
-}
 	?>
