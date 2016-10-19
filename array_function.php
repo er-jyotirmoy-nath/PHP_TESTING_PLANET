13ff51ce25e40fa9890e68b0210cac09b15be01b
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-19 12:38:06 +0530

    Update array_function.php

--- array_function.php
+++ array_function.php
@@ -1,1 +1,18 @@
 
+<?php
+$array_name = array("orange","apple","grapes","bannna");
+
+array_push($array_name,"Cucumber");
+print_r($array_name);
+echo '</br>';
+$fruit = array_pop($array_name);
+print_r($array_name);
+echo '</br>';
+array_shift($array_name,"Mango");
+print_r($array_name);
+echo '</br>';
+$fruit = array_unshift($array_name);
+print_r($fruit);
+echo '</br>';
+
+?>
