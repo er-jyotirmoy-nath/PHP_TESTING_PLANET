76b061d961a85c5d4f661122286727f983d86188
jyotirmoy-err-2016 <er.jyotirmoy.nath@gmail.com>
2016-10-20 07:55:50 +0530

    PHP_NFO

--- php_oracle_conn.php
+++ php_oracle_conn.php
@@ -1,46 +1,44 @@
 <?php
     $user='root'; // Enter your DB User Name.
-    $pass=''; // Enter your DB Password.
-    $dataBaseName='webexpertdeveloper'; // Enter your Database Name.
+    $pass='root'; // Enter your DB Password.
+    $dataBaseName='a_database'; // Enter your Database Name.
     $dbh = new PDO('OCI:dbname='.$dataBaseName.'charset=UTF-8', $user, $pass);
     echo "Connection Successful";
 ?>
 
-//or
+
 
 <?php
     try
     {
         $user='root'; // Enter your DB User Name.
-        $pass=''; // Enter your DB Password.
-        $dataBaseName='webexpertdeveloper'; // Enter your Database Name.
+        $pass='root'; // Enter your DB Password.
+        $dataBaseName='a_database'; // Enter your Database Name.
         $dbh = new PDO('OCI:dbname='.$dataBaseName.'charset=UTF-8', $user, $pass);
         echo "Connection Successful";
     }
     catch (PDOException $e)
     {
-        print "Error!: " . $e->getMessage() . "
-";
+        print "Error!: " . $e->getMessage() . "";
         die();
     }
 ?>
 
-//or
+
 
 <?php
-    try
+/*    try
     {
         $user='root'; // Enter your DB User Name.
-        $pass=''; // Enter your DB Password.
-        $dataBaseName='webexpertdeveloper'; // Enter your Database Name.
+        $pass='root'; // Enter your DB Password.
+        $dataBaseName='a_database'; // Enter your Database Name.
         $dbh = new PDO('OCI:dbname='.$dataBaseName.'charset=UTF-8', $user, $pass,);
         echo "Connection Successful";
                 $dbh = null; // It Will Close the connection
     }
     catch (PDOException $e)
     {
-        print "Error!: " . $e->getMessage() . "
-";
+        print "Error!: " . $e->getMessage() . "";
         die();
-    }
+    }*/
 ?>
