ceb824e9949e0d178a72231db207f016f002ca51
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-22 15:59:31 +0530

    Update array_function.php

--- array_function.php
+++ array_function.php
@@ -1,5 +1,33 @@
 
 <?php
+
+
+/*a real life example*/
+class array_work
+{
+	public function convert_to_city($city_assoc,$newcity)
+	{
+		//$newcity = $city;
+		$count = 0;
+		foreach($city_assoc as $row => $value)
+		{
+			$newcity[strtolower($row)] = 0;
+		}
+		foreach($city_assoc as $row => $value)
+		{
+			if(strtolower($row) == strtolower($row))
+			{
+				$newcity[strtolower($row)] += $value;
+			}
+		}
+		print_r($newcity);
+	}
+}
+$mywork = new array_work();
+$city = array("mumbai" => 5, "Mumbai" => 10,"Delhi" => 15,"DELHI" => 20,"madras" => 15,"Madras" => 5,"MADRAS" => 5,"MaDrAs" => 20);
+$newcity = array();
+$mywork->convert_to_city($city,$newcity);
+
 $array_name = array("orange","apple","grapes","bannna");
 
 array_push($array_name,"Cucumber");
@@ -16,30 +44,4 @@ array_unshift($array_name,"Mango");
 print_r($array_name);
 echo '</br>';
 echo array_key_exists(3, $array_name);
-
-/*a real life example*/
-<?php
-$city = array("mumbai" => 5, "Mumbai" => 10,"Delhi" => 15,"DELHI" => 20,"madras" => 15,"Madras" => 5,"MADRAS" => 5);
-$newcity = array();
-//$newcity = $city;
-$count = 0;
-
-foreach($city as $row => $value)
-{
-	$newcity[strtolower($row)] = 0;
-	
-	
-}
-foreach($city as $row => $value)
-{
-	
-	if(strtolower($row) == strtolower($row))
-	{
-		$newcity[strtolower($row)] += $value;
-	}
-	
-	
-}
-print_r($newcity);
-?>
 ?>
