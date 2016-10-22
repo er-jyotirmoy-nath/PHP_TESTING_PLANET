555439abc736ae87b0ac0cc3d1aeaeaecc6719d1
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-22 15:45:33 +0530

    Update array_function.php

--- array_function.php
+++ array_function.php
@@ -16,4 +16,30 @@ array_unshift($array_name,"Mango");
 print_r($array_name);
 echo '</br>';
 echo array_key_exists(3, $array_name);
+
+/*a real life example*/
+<?php
+$city = array("mumbai" => 5, "Mumbai" => 10,"Delhi" => 15,"DELHI" => 20,"madras" => 15,"Madras" => 5,"MADRAS" => 5);
+$newcity = array();
+//$newcity = $city;
+$count = 0;
+
+foreach($city as $row => $value)
+{
+	$newcity[strtolower($row)] = 0;
+	
+	
+}
+foreach($city as $row => $value)
+{
+	
+	if(strtolower($row) == strtolower($row))
+	{
+		$newcity[strtolower($row)] += $value;
+	}
+	
+	
+}
+print_r($newcity);
+?>
 ?>
