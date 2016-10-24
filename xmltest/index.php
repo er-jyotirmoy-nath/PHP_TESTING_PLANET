54014c3c37899090a7d2482df0dfba18be767945
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-24 16:32:36 +0530

    Update index.php

--- xmltest/index.php
+++ xmltest/index.php
@@ -5,7 +5,10 @@ $xml = simplexml_load_file("myxml.xml");
 echo $xml->producer[0]->name;
 echo $xml->producer[0]->age;
 
-foreach($xml->producer as $
+foreach($xml->producer as $pdn)
+{
+  echo 'Name'.$pdn->name.' Age'.$pdn->age.'<br>'; 
+}
 
 
 
