54fef23eae730f3d99075cb34da01530876cb068
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2016-10-25 16:47:55 +0530

    Update image_create.php

--- image_create.php
+++ image_create.php
@@ -1,1 +1,20 @@
 
+<?php
+
+header("Content-type: image/jpeg");
+$name = "jyotirmoy85@gmail.com";
+$stringlen = strlen($name);
+$fontheight = 10;
+$image_height = ImageFontHeight($fontheight);
+$image_width = ImageFontWidth($fontheight) * $stringlen;
+
+$image = imagecreate($image_width,$image_height);
+
+imagecolorallocate($image,255,255,255);
+
+$font_color = imagecolorallocate($image,0,0,0);
+
+imagestring($image,$fontheight,0,0,$name,$font_color);
+imagejpeg($image);
+
+?>
